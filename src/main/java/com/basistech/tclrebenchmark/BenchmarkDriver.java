5917ad760e06a36ac94c5b4ad22ce647111f700b
Benson Margulies <benson@basistech.com>
2015-01-21 10:38:39 -0500

    Initial pass at the code.

---
+++ src/main/java/com/basistech/tclrebenchmark/BenchmarkDriver.java
@@ -0,0 +1,110 @@
+/******************************************************************************
+ ** This data and information is proprietary to, and a valuable trade secret
+ ** of, Basis Technology Corp.  It is given in confidence by Basis Technology
+ ** and may only be used as permitted under the license agreement under which
+ ** it has been distributed, and in no other way.
+ **
+ ** Copyright (c) 2015 Basis Technology Corporation All rights reserved.
+ **
+ ** The technical data and information provided herein are provided with
+ ** `limited rights', and the computer software provided herein is provided
+ ** with `restricted rights' as those terms are defined in DAR and ASPR
+ ** 7-104.9(a).
+ ******************************************************************************/
+
+package com.basistech.tclrebenchmark;
+
+import com.basistech.tclre.HsrePattern;
+import com.basistech.tclre.PatternFlags;
+import com.basistech.tclre.RePattern;
+import com.basistech.tclre.RegexException;
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * CLI to run the benchmark
+ */
+public class BenchmarkDriver {
+
+    enum Task {
+        find
+    }
+
+    @Argument(required = true)
+    Task task;
+
+    @Argument(index = 1, required = true)
+    File text;
+
+    @Argument(index = 2, required = true)
+    List<File> regexes;
+
+    String textContent;
+    List<RePattern> patterns;
+
+    public static void main(String[] args) {
+        new BenchmarkDriver().doMain(args);
+    }
+
+    private void doMain(String[] args) {
+        CmdLineParser parser = new CmdLineParser(this);
+        parser.setUsageWidth(80);
+
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            System.err.println("BenchmarkDriver TASK textFile patternFile1 ... patternFileN");
+            parser.printUsage(System.err);
+            System.err.println();
+            System.exit(1);
+        }
+
+        try {
+            setupInputs();
+        } catch (IOException e) {
+            System.err.println("Error reading text or regexes");
+            e.printStackTrace(System.err);
+            System.exit(1);
+            return;
+        } catch (RegexException e) {
+            System.err.println("Error parsing regex");
+            e.printStackTrace(System.err);
+            System.exit(1);
+            return;
+        }
+
+        // for now we only have one task.
+        switch (task) {
+        case find:
+            doFind();
+        default:
+            break;
+        }
+    }
+
+    private void setupInputs() throws IOException, RegexException {
+        textContent = Files.toString(text, Charsets.UTF_8);
+        for (File regexFile : regexes) {
+            List<String> regexLines = Files.readLines(regexFile, Charsets.UTF_8);
+            // each one is three fields separated by tabs, but with no escaping on the last field
+            for (String line : regexLines) {
+                int tx = line.indexOf('\t');
+                tx = line.indexOf('\t', tx + 1);
+                String regex = line.substring(tx);
+                patterns.add(HsrePattern.compile(regex, PatternFlags.ADVANCED));
+            }
+        }
+    }
+
+    private void doFind() {
+        new FindBenchmark(patterns, textContent).run();
+    }
+}
---
+++ src/main/java/com/basistech/tclrebenchmark/FindBenchmark.java
@@ -0,0 +1,72 @@
+/******************************************************************************
+ ** This data and information is proprietary to, and a valuable trade secret
+ ** of, Basis Technology Corp.  It is given in confidence by Basis Technology
+ ** and may only be used as permitted under the license agreement under which
+ ** it has been distributed, and in no other way.
+ **
+ ** Copyright (c) 2015 Basis Technology Corporation All rights reserved.
+ **
+ ** The technical data and information provided herein are provided with
+ ** `limited rights', and the computer software provided herein is provided
+ ** with `restricted rights' as those terms are defined in DAR and ASPR
+ ** 7-104.9(a).
+ ******************************************************************************/
+
+package com.basistech.tclrebenchmark;
+
+import com.basistech.tclre.ReMatcher;
+import com.basistech.tclre.RePattern;
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Measure the speed of find().
+ * Find is is the easy case: given a slug of text and a collection of expressions,
+ * we can call find on each one add up the times. matches() and lookingAt() are trickier
+ * as we might need to select many different regions of text get good coverage.
+ */
+public class FindBenchmark {
+    private final MetricRegistry metrics = new MetricRegistry();
+    private final Timer responses = metrics.timer("find");
+    private final List<RePattern> patterns;
+    private final String text;
+    private ConsoleReporter reporter;
+
+
+    public FindBenchmark(List<RePattern> patterns, String text) {
+        this.patterns = patterns;
+        this.text = text;
+    }
+
+    public void run() {
+        startReport();
+        for (RePattern pattern : patterns) {
+            ReMatcher matcher = pattern.matcher(text);
+            final Timer.Context context = responses.time();
+            try {
+                matcher.find();
+            } finally {
+                context.stop();
+            }
+        }
+        stopReport();
+    }
+
+    private void stopReport() {
+        reporter.report();
+        reporter.stop();
+    }
+
+    private void startReport() {
+        reporter = ConsoleReporter.forRegistry(metrics)
+                .convertRatesTo(TimeUnit.SECONDS)
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                .build();
+        reporter.start(10, TimeUnit.SECONDS); // generally have nothing to say until the end
+    }
+
+}
